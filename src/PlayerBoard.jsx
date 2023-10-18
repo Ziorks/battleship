48553878c38ad88e7227600e9f7bf815a083b594
Frank Scott <franksixer@gmail.com>
2023-10-18 09:01:33 -0500

    added state to spaces and consolidated css

--- src/PlayerBoard.jsx
+++ src/PlayerBoard.jsx
@@ -1,19 +1,26 @@
 import { PlayerGridSpace } from "./PlayerGridSpace";
-import "./PlayerBoard.css";
 
 const playerGrid = [];
 for (let row = 0; row < 11; row++) {
   for (let column = 0; column < 11; column++) {
+    const rowLabel = 10 - row;
+    const columnLabel = String.fromCharCode(column + 64);
     if (row === 10 && column === 0) {
-      playerGrid.push(<div className="gridSpace"></div>);
+      playerGrid.push(<div key="corner" className="gridSpace"></div>);
     } else if (row === 10 && column !== 0) {
       playerGrid.push(
-        <p className="gridSpace">{String.fromCharCode(column + 64)}</p>
+        <p key={"column" + columnLabel} className="gridSpace">
+          {columnLabel}
+        </p>
       );
     } else if (column === 0) {
-      playerGrid.push(<p className="gridSpace">{10 - row}</p>);
+      playerGrid.push(
+        <p key={"row" + rowLabel} className="gridSpace">
+          {rowLabel}
+        </p>
+      );
     } else {
-      playerGrid.push(<PlayerGridSpace />);
+      playerGrid.push(<PlayerGridSpace key={columnLabel + rowLabel} />);
     }
   }
 }
--- src/PlayerGridSpace.jsx
+++ src/PlayerGridSpace.jsx
@@ -1,8 +1,17 @@
-import "./PlayerGridSpace.css";
+import { useState } from "react";
 
-export function PlayerGridSpace() {
+export function PlayerGridSpace({ key }) {
+  const [state, setState] = useState({ bombed: false, ship: false });
+
+  function handleClick() {
+    setState({ ...state, bombed: true });
+  }
+
   return (
-    <div className="gridSpace">
+    <div
+      className={"gridSpace playerGridSpace" + (state.bombed ? " bombed" : "")}
+      onClick={handleClick}
+    >
       <i class="fa-regular fa-circle-dot"></i>
     </div>
   );
