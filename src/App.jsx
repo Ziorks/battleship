3e1b87ea8f57dbe91e8a8b545888afe4f2090d3e
Frank Scott <franksixer@gmail.com>
2023-10-23 00:53:14 -0500

    ship placement works (with jank rotate too)

--- src/App.jsx
+++ src/App.jsx
@@ -13,41 +13,75 @@ const ships = [
 function App() {
   const [state, setState] = useState({
     remainingShips: ships.length,
+    horizontal: false,
     playerTurn: true,
   });
+
   const [playerBoard, setPlayerBoard] = useState(generateBoardArray());
 
+  //This works but is real slow to update
+  //
+  // window.addEventListener("keydown", (e) => {
+  //   if (e.key === "r") {
+  //     setState((currentState) => {
+  //       return { ...currentState, horizontal: !currentState.horizontal };
+  //     });
+  //   }
+  // });
+
   function placeShip() {
-    setState((currentState) => {
-      return {
-        ...currentState,
-        remainingShips: currentState.remainingShips - 1,
-      };
-    });
-    setPlayerBoard((currentBoard) => {
-      return currentBoard.map((tile) => {
-        if (tile.placingShip) {
-          return { ...tile, ship: true };
-        } else {
-          return tile;
-        }
+    if (
+      !playerBoard.reduce(
+        (accum, tile) => accum || (tile.ship && tile.placingShip),
+        false
+      )
+    ) {
+      setState((currentState) => {
+        return {
+          ...currentState,
+          remainingShips: currentState.remainingShips - 1,
+        };
       });
-    });
+      setPlayerBoard((currentBoard) => {
+        return currentBoard.map((tile) => {
+          if (tile.placingShip) {
+            return { ...tile, ship: true, placingShip: false };
+          } else {
+            return tile;
+          }
+        });
+      });
+    }
   }
 
   function handleHover(row, column) {
     if (state.remainingShips > 0) {
       setPlayerBoard((currentBoard) => {
         return currentBoard.map((tile) => {
-          if (
-            tile.row === row &&
-            tile.column >= column &&
-            tile.column.charCodeAt() <
-              column.charCodeAt() + ships[state.remainingShips - 1].size
-          ) {
-            return { ...tile, placingShip: true };
+          if (state.horizontal) {
+            const shipLength = ships[state.remainingShips - 1].size;
+            if (
+              tile.row === row &&
+              tile.column >= column &&
+              tile.column.charCodeAt() < column.charCodeAt() + shipLength &&
+              column.charCodeAt() + shipLength <= 75
+            ) {
+              return { ...tile, placingShip: true };
+            } else {
+              return { ...tile, placingShip: false };
+            }
           } else {
-            return { ...tile, placingShip: false };
+            const shipLength = ships[state.remainingShips - 1].size;
+            if (
+              tile.column === column &&
+              tile.row >= row &&
+              tile.row < row + shipLength &&
+              row + shipLength <= 11
+            ) {
+              return { ...tile, placingShip: true };
+            } else {
+              return { ...tile, placingShip: false };
+            }
           }
         });
       });
@@ -69,6 +103,7 @@ function App() {
             {" - "}
             {ships[state.remainingShips - 1].size} spaces
           </p>
+          <p>press 'r' to rotate</p>
         </div>
       )}
     </>
